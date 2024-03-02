4d9968987d408f3e865269d9bfe9b160619a75db
carloscabral656 <edu.carlos.cec@gmail.com>
2024-03-02 17:53:54 -0300

    Creating message for login form

--- src/app/slices/login/features/login-interface/login-interface.component.ts
+++ src/app/slices/login/features/login-interface/login-interface.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { LoginUserDataComponent } from '../../presentation/login-user-data/login-user-data.component';
+import { LoginMessageComponent } from '../../presentation/login-message/login-message.component';
 
 @Component({
   selector: 'app-login-interface',
   standalone: true,
-  imports: [MatGridListModule, LoginUserDataComponent],
+  imports: [MatGridListModule, LoginUserDataComponent, LoginMessageComponent],
   templateUrl: './login-interface.component.html',
   styleUrl: './login-interface.component.scss'
 })

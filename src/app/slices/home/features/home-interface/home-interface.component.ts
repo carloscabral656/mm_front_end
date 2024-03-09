e3d179d30205ed19c28fe5e6cfc8f42d46857e9a
carloscabral656 <edu.carlos.cec@gmail.com>
2024-03-09 18:40:38 -0300

    Creating home component

--- src/app/slices/home/features/home-interface/home-interface.component.ts
+++ src/app/slices/home/features/home-interface/home-interface.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-home-interface',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule],
   templateUrl: './home-interface.component.html',
   styleUrl: './home-interface.component.scss'
 })
 export class HomeInterfaceComponent {
-
-}
+  public showFiller = false;
+} 
--- src/app/slices/login/routes/routes.ts
+++ src/app/slices/login/routes/routes.ts
@@ -2,7 +2,6 @@ import { Routes } from "@angular/router";
 import { LoginInterfaceComponent } from "../features/login-interface/login-interface.component";
 
 export const loginRoutes: Routes = [
-
     {
         path: '', 
         component: LoginInterfaceComponent

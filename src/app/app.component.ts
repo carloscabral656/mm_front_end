85267a29441ae43e2fd156d013f4d53736a3f575
carloscabral656 <edu.carlos.cec@gmail.com>
2024-03-28 20:59:04 -0300

    Creating user and notification icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'app-root',
--- src/app/slices/main/presentation/header-menu/header-menu.component.ts
+++ src/app/slices/main/presentation/header-menu/header-menu.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { HamburguerComponent } from '../../../shared/components/hamburguer/hamburguer.component';
 import { MatDrawer } from '@angular/material/sidenav';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header-menu',
   standalone: true,
-  imports: [HamburguerComponent],
+  imports: [HamburguerComponent, MatIcon],
   templateUrl: './header-menu.component.html',
   styleUrl: './header-menu.component.scss'
 })
